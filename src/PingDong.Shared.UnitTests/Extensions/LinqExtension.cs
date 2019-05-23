8607dcb1540077f66af0626b524633cf14fd322b
Ping Dong <ping.dong@parallo.com>
2019-05-23 19:39:28 +1200

    Initial Checkin

---
+++ src/PingDong.Shared.UnitTests/Extensions/LinqExtension.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using PingDong.Linq;
+using Xunit;
+
+namespace PingDong.Linq
+{
+    public class LinqExtensionTests
+    {
+        [Fact]
+        public void Null_ShouldReturn_True()
+        {
+            List<string> source = null;
+            Assert.True(source.IsNullOrEmpty());
+        }
+
+        [Fact]
+        public void Empty_ShouldReturn_True()
+        {
+            var source = new List<string>();
+            Assert.True(source.IsNullOrEmpty());
+        }
+
+        [Fact]
+        public void List_ShouldReturn_True()
+        {
+            var source = new List<string> { "Test" };
+            Assert.False(source.IsNullOrEmpty());
+        }
+    }
+}
---
+++ src/PingDong.Shared/Extensions/LinqExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PingDong.Linq
+{
+    public static class LinqExtension
+    {
+        /// <summary>
+        /// Is the source null or empty.
+        /// </summary>
+        /// <typeparam name="T">Type of element</typeparam>
+        /// <param name="source">A list of elements</param>
+        /// <returns>If the given list is null or empty, this method return True, otherwise False</returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source == null || !source.Any();
+        }
+    }
+}
