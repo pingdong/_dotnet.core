6e0fe65e79a7c1bfbd24dfe533e6e521e479f019
Ping Dong <ping.dong@live.com>
2019-07-13 23:03:22 +1200

    Add extension for turning first char upper or lower

---
+++ src/PingDong.Shared.UnitTests/Extensions/StringExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using PingDong.Linq;
+using Xunit;
+
+namespace PingDong.Shared
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void FirstCharToUpper()
+        {
+            var st = "test";
+
+            var st1 = st.FirstCharToUpper();
+            Assert.Equal("Test", st1);
+
+            var st2 = st1.FirstCharToUpper();
+            Assert.Equal("Test", st1);
+        }
+        [Fact]
+        public void FirstCharToUpper_IfStringIsNullOrEmpty()
+        {
+            Assert.Throws<ArgumentNullException>(() => "".FirstCharToUpper());
+            Assert.Throws<ArgumentNullException>(() => " ".FirstCharToUpper());
+        }
+
+        [Fact]
+        public void FirstCharToLower()
+        {
+            var st = "Test";
+
+            var st1 = st.FirstCharToLower();
+            Assert.Equal("test", st1);
+
+            var st2 = st1.FirstCharToLower();
+            Assert.Equal("test", st1);
+        }
+        [Fact]
+        public void FirstCharToLower_IfStringIsNullOrEmpty()
+        {
+            Assert.Throws<ArgumentNullException>(() => "".FirstCharToLower());
+            Assert.Throws<ArgumentNullException>(() => " ".FirstCharToLower());
+        }
+    }
+}
---
+++ src/PingDong.Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PingDong.Shared
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Change to first character in the given string to upper case
+        /// </summary>
+        /// <param name="value">the given string</param>
+        /// <returns></returns>
+        public static string FirstCharToUpper(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value));
+
+            return char.ToUpper(value[0]) + value.Substring(1);
+        }
+
+        /// <summary>
+        /// Change to first character in the given string to upper case
+        /// </summary>
+        /// <param name="value">the given string</param>
+        /// <returns></returns>
+        public static string FirstCharToLower(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value));
+
+            return char.ToLower(value[0]) + value.Substring(1);
+        }
+    }
+}
